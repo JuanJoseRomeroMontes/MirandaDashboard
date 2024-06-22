70e8e8e9a0b81f240db369ca01d549967d043787
Juan Jose Romero Montes <claseinfojuanjose@gmail.com>
2024-06-22 16:17:25 +0200

    Feature: table tabs filtering

--- src/pages/bookings.jsx
+++ src/pages/bookings.jsx
@@ -2,7 +2,7 @@ import bookings from '../data/bookingsData.json';
 import rooms from '../data/roomsData.json';
 import { Menus } from '../components/Menus/menus';
 import { Table } from '../components/Tables/Table';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Guest, RoomStatus, SpecialRequest, DeleteBookingData } from '../components/Tables/BookingTableComponents';
 import styled from 'styled-components';
 
@@ -26,37 +26,63 @@ const RequestPopUp = styled.div`
     }
 `;
 
+const FilterTab = styled.div`
+    padding: 15px;
+    border: none;
+    border-bottom: 1px solid ${(props) => (props.$selected ? "#135846" : "#D4D4D4")};
+    color: ${(props) => (props.$selected ? "#135846" : "#6E6E6E")};
+`;
+
 export const BookingsPage = () => {
-    const [bookingData, setBookingData] = useState(() => {
-        const data = [];
-
-        bookings.forEach(element => {
-            const modifiedElement = { ...element };
-            const bookedRoom = rooms.find(room => room.id === modifiedElement.roomId);
-            
-            modifiedElement.roomType = bookedRoom.roomType;
-            modifiedElement.roomNumber = bookedRoom.roomNumber;
-            modifiedElement.status = CheckDay(modifiedElement.checkIn, modifiedElement.checkOut);
-
-            data.push(modifiedElement)
-        });
-
-        return data;
-    })
-
-    const [dataTemp, setDataTemp] = useState(...bookingData)
-
+    const [bookingData, setBookingData] = useState(getBookingData(bookings, rooms))
     const [popUpMessage, setpopUpMessage] = useState("");
+    const [tabsState, setTabsState] = useState([true, false, false, false])
+    const [order, setOrder] = useState("");
+    const [inputValue, setInputValue] = useState("");
+    const [filter, setFilter] = useState({defaultFilter: true}); //object with properties: property, value
+    const [search, setSearch] = useState({defaultOrder: true}); //object with properties: property, value
 
-    const ShowPopUp = (message) => {
+    const filteredBookings = useMemo(() => {
+        let newBookingsList = bookingData;
+
+        if(!(filter.defaultFilter))
+            newBookingsList = bookingData.filter(booking => booking[filter.property] == filter.value);
+
+    /*  newBookingsList = newBookingsList.filter(booking => booking[search.property].includes(search.property));*/
+        
+        if(!(order.defaultOrder))
+        {
+            newBookingsList.sort((a,b) => {
+                if(a[order.property] < b[order.property])
+                    return 1;
+                else if(a[order.property] > b[order.property])
+                    return -1;
+                return 0;
+            });
+        }
+        return newBookingsList;
+    }, [order, filter, search])
+
+    function handlectiveTab(newActiveTab){
+        let newTabsState = [false, false, false, false]
+    
+        newTabsState[newActiveTab] = true;
+    
+        setTabsState(newTabsState);
+    }
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handlePopUp = (message) => {
         setpopUpMessage(message)
     }
 
-    const DeleteData = (idToFilter) => {
-        const FilteredData = [...bookingData].filter(booking => booking.id !== idToFilter);
-        console.log(FilteredData)
-        setBookingData(FilteredData)
-        setDataTemp(FilteredData)
+    const handleDeleteBooking = (idToFilter) => {
+        const deletedData = [...bookingData].filter(booking => booking.id !== idToFilter);
+        setBookingData(deletedData)
+        setDataTemp(deletedData)
     }
 
     const columns = [
@@ -64,32 +90,70 @@ export const BookingsPage = () => {
         { header: 'Order Date', render: (row) => <p>{row.bookDate}</p>, },
         { header: 'Check In', render: (row) => <p>{row.checkIn}</p>, },
         { header: 'Check Out', render: (row) => <p>{row.checkOut}</p>, },
-        { header: 'Special Request', render: (row) => <SpecialRequest message={row.specialRequest} showPopUp={ShowPopUp}/>, },
+        { header: 'Special Request', render: (row) => <SpecialRequest message={row.specialRequest} handlePopUp={handlePopUp}/>, },
         { header: 'Room Type', render: (row) => <p>{row.roomType}</p>, },
         { header: 'Status', render: (row) => <RoomStatus status={row.status}/>, },
-        { header: '',  render: (row) => <DeleteBookingData id={row.id} deleteFunc={DeleteData}/>, },
+        { header: '',  render: (row) => <DeleteBookingData id={row.id} deleteFunc={handleDeleteBooking}/>, },
     ];
 
     return(
         <>
             <Menus title="Bookings">
-                <h1>Bookings</h1>
-                <Table data={bookingData} columns={columns} />
-                
+                <div style={{padding: "15px"}}>
+                    <div style={{display: "inline-flex"}}>
+                        <FilterTab $selected={tabsState[0]} onClick={() => {
+                            handlectiveTab(0); 
+                            setFilter({defaultFilter: true}); //DefaultFilter value doens't matter, only if the property exist or not
+                            setOrder({defaultOrder: true}); //DefaultOrder value doens't matter, only if the property exist or not
+                        }}>All Bookings</FilterTab>
+                        <FilterTab $selected={tabsState[1]} onClick={() => {
+                            handlectiveTab(1);
+                            setFilter({defaultFilter: true}); 
+                            setOrder({property: "checkIn"});
+                        }} >Checking In</FilterTab>
+                        <FilterTab $selected={tabsState[2]} onClick={() => {
+                            handlectiveTab(2);
+                            setFilter({defaultFilter: true}); 
+                            setOrder({property: "checkOut"});
+                            }}>Checking Out</FilterTab>
+                        <FilterTab $selected={tabsState[3]} onClick={() => {
+                            handlectiveTab(3);
+                            setFilter({property: "status", value: "Check in"});
+                            setOrder({property: "bookDate"});
+                            }}>In Progress</FilterTab>
+                    </div>
+                    <input type="text" value={inputValue} onChange={handleInputChange} />
+                    <Table data={filteredBookings} columns={columns} />
+                </div>
             </Menus>
             <RequestPopUp $display={popUpMessage!==""} onClick={() => {setpopUpMessage("")}}><div>{popUpMessage}</div></RequestPopUp>
         </>
     )
 };
 
-function CheckDay(checkIn, checkOut){
+function getBookingData(bookingList, roomList){
+    const data = [];
+
+    bookingList.forEach(element => {
+        const modifiedElement = { ...element };
+        const bookedRoom = roomList.find(room => room.id === modifiedElement.roomId);
+        
+        modifiedElement.roomType = bookedRoom.roomType;
+        modifiedElement.roomNumber = bookedRoom.roomNumber;
+        modifiedElement.status = checkDay(modifiedElement.checkIn, modifiedElement.checkOut);
+
+        data.push(modifiedElement)
+    });
+
+    return data;
+}
+
+function checkDay(checkIn, checkOut){
     let roomState = null;
     const date = new Date();
-    const checkInParts = checkIn.split('-');
-    const checkOutParts = checkOut.split('-');
 
-    const checkInDate = new Date(parseInt(checkInParts[0]), parseInt(checkInParts[1])-1, parseInt(checkInParts[2])); // Meses en JavaScript son indexados desde 0, por eso le resto 1
-    const checkOutDate = new Date(parseInt(checkOutParts[0]), parseInt(checkOutParts[1])-1, parseInt(checkOutParts[2]));
+    const checkInDate = convertStringToDate(checkIn);
+    const checkOutDate = convertStringToDate(checkOut);
 
     if (date < checkInDate) 
         roomState = 'In progress'
@@ -100,3 +164,15 @@ function CheckDay(checkIn, checkOut){
 
     return roomState;
 }
+
+function convertStringToDate(date){
+    const dateSplit = date.split('-');
+    //Javascript months are indexed from 0, thats why they are substracted 1
+    return new Date(parseInt(dateSplit[0]), parseInt(dateSplit[1])-1, parseInt(dateSplit[2]));
+}
+
+/* if (search.property && search.value) {
+    newBookingsList = newBookingsList.filter(booking => 
+        booking[search.property] && booking[search.property].toLowerCase().includes(search.value.toLowerCase())
+    );
+} */
