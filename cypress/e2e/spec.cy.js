a57653b4b4c24246d071f608d336dbf476a8670c
Juan Jose Romero Montes <claseinfojuanjose@gmail.com>
2024-07-03 16:43:29 +0200

    Feature: cypress testing

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,25 @@
+describe('My First Test', () => {
+  it('Correct data', () => {
+    cy.visit('http://localhost:5173/')
+
+    cy.url().should('include', '/login')
+
+    cy.get('[id^=email]').type('miranda@gmail.com')
+    cy.get('[id^=password]').type('mirapass')
+    cy.get('form').submit()
+
+    cy.url().should('eq', 'http://localhost:5173/')
+  })
+
+  it('Incorrect data', () => {
+    cy.visit('http://localhost:5173/')
+
+    cy.url().should('include', '/login')
+
+    cy.get('[id^=email]').type('miranda@gmail.com')
+    cy.get('[id^=password]').type('invalidPassword')
+    cy.get('form').submit()
+
+    cy.url().should('include', '/login')
+  })
+})
